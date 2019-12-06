1ffdf2f03bab337698d3e62c6885168032c15c51
demogorgon22 <sammagnus22@yahoo.com>
2019-12-06 13:29:25 -0500

    More work.

--- public/scoreboard.js
+++ public/scoreboard.js
@@ -1,5 +1,45 @@
 
+let table;
+const init = ()=>{
+	table = document.getElementById('table');
+	table.innerHTML = "<td>ayudame</td>"
+	updateTable();
+}
+
+const updateTable = ()=>{
+	table.innerHTML = '';
+	getScores(drawScores);
+}
+
+const drawScores = teams=>{
+	console.log(teams);
+	teams.forEach(team=>{
+		drawTeam(team);
+	});
+}
+
+
+const drawTeam = team=>{
+	const tr = document.createElement('tr');
+	const teamName = document.createElement('td');
+	teamName.innerText = team.name;
+	tr.appendChild(teamName);
+	table.appendChild(tr);
+	
+}
+
+const getScores = callback=>{
+	fetch('/api/getScoreboard')
+		.then(res=>res.json())
+		.then(data=>{
+			callback(data);
+		})
+		.catch((err)=>{
+			console.log(err);
+		});
+}
+
 
 window.addEventListener('load',()=>{
-
+	init();
 });
