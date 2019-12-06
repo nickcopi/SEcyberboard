b4abe689dc78835aa3e35c614280f6d98d1c0c71
demogorgon22 <sammagnus22@yahoo.com>
2019-12-06 13:43:35 -0500

    Closer to working.

--- public/scoreboard.js
+++ public/scoreboard.js
@@ -2,7 +2,6 @@
 let table;
 const init = ()=>{
 	table = document.getElementById('table');
-	table.innerHTML = "<td>ayudame</td>"
 	updateTable();
 }
 
@@ -13,19 +12,56 @@ const updateTable = ()=>{
 
 const drawScores = teams=>{
 	console.log(teams);
+	if(teams[0]) drawHeader(teams[0]);
 	teams.forEach(team=>{
 		drawTeam(team);
 	});
 }
 
+const drawHeader = team=>{
+	const imageHeader = `
+		<td>Image</td>
+		<td>Findings</td>
+		<td>Penalties</td>
+		<td>Score</td>
+	`
+	table.innerHTML += `
+		<tr>
+			<td>Name</td>
+			<td>Tier</td>
+			<td>Time</td>
+			<td>Score</td>
+			${imageHeader.repeat(team.images.length)}
+
+		</tr>
+	`;
+}
 
 const drawTeam = team=>{
-	const tr = document.createElement('tr');
-	const teamName = document.createElement('td');
-	teamName.innerText = team.name;
-	tr.appendChild(teamName);
-	table.appendChild(tr);
+	table.innerHTML += `
+		<tr>
+			<td>${team.name}</td>
+			<td>${team.division}</td>
+			<td>${team.spentTime}</td>
+			<td>${team.totalScore}</td>
+			${team.images.map(image=>getImageData(image))}
+		</tr>
+	`;
+	//const tr = document.createElement('tr');
+	//const teamName = document.createElement('td');
+	//teamName.innerText = team.name;
+	//tr.appendChild(teamName);
+	//table.appendChild(tr);
 	
+}
+
+const getImageData = image =>{
+	return `
+		<td>${image.name}</td>
+		<td>${image.finds}</td>
+		<td>${image.penalties}</td>
+		<td>${image.score}</td>
+	`
 }
 
 const getScores = callback=>{
